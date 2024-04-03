6e18feb7acf08fb249a30482e8a6fc6211258287
xdshka <pyfigchannel@gmail.com>
2024-04-03 16:39:06 +0700

    mv 7containers to scrollview

---
+++ lab3.04/scrollview/7containers/_containersApp.swift
@@ -0,0 +1,17 @@
+//
+//  _containersApp.swift
+//  7containers
+//
+//  Created by user on 03.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct _containersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
