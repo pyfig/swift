e7e5c6dad3b1d70b8975dfaf18d50fd5ae6bdf8a
xdshka <pyfigchannel@gmail.com>
2024-04-03 16:43:53 +0700

    added project

---
+++ lab3.04/LazyVGridNLazyHGrid/lazyVgridNLazyHGrid/lazyVgridNLazyHGrid/lazyVgridNLazyHGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazyVgridNLazyHGridApp.swift
+//  lazyVgridNLazyHGrid
+//
+//  Created by user on 03.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct lazyVgridNLazyHGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
