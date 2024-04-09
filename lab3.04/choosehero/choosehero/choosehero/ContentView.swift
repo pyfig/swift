d4da35b31c80945020d99eff28d4d5cf95482f4e
xdshka <pyfigchannel@gmail.com>
2024-04-09 16:06:53 +0700

    tema8

--- lab3.04/choosehero/choosehero/choosehero/ContentView.swift
+++ lab3.04/choosehero/choosehero/choosehero/ContentView.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct Item: View {
+    var img: String
+    var body: some View{
+        VStack{
+            Image(img)
+                .resizable()
+                .frame(width: 50, height: 100)
+                .aspectRatio(contentMode: .fit)
+            Text(img)
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    private var columns: [GridItem] = [
+        GridItem(.adaptive(minimum: 50, maximum: 100))
+    ]
+    
+    let images = ["ashe", "zen", "reaper", "dva"]
+    var body: some View {
+        ScrollView {
+            LazyVGrid(
+                columns: columns,
+                alignment: .center,
+                spacing: 16,
+                pinnedViews: [.sectionHeaders, .sectionFooters]
+            ) {
+                ForEach(0...10, id: \.self) {index in
+                    Item(img: images[index % images.count])
+                }
+                .frame(width: UIScreen.main.bounds.width - 32, height: 200)
+            }
+        }
     }
 }
 
---
+++ tema8/tema8/tema8App.swift
@@ -0,0 +1,17 @@
+//
+//  tema8App.swift
+//  tema8
+//
+//  Created by user on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct tema8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
